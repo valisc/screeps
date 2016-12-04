d619254c403f562c386eab004a7117be8510f22c
Valis C <valis@inkanito.net>
2016-12-04 15:54:35 -0500

    create harvester role

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
+var roleHarvester = require('role.harvester');
+
 module.exports.loop = function () {
   for(var name in Game.creeps) {
     var creep = Game.creeps[name];
-
-    if(creep.carry.energy < creep.carryCapacity) {
-      var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
-      }
-    }
-    else {
-      if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns['Spawn1']);
-      }
-    }
+    roleHarvester.run(creep);
   }
 }
