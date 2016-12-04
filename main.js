f91fc8a355d03a1a8874d46b30849a798231847e
Valis C <valis@inkanito.net>
2016-12-04 14:29:20 -0500

    Add second screep

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 module.exports.loop = function () {
-  var creep = Game.creeps['Harvester1'];
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
 
-  if(creep.carry.energy < creep.carryCapacity) {
-    var sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-      creep.moveTo(sources[0]);
+    if(creep.carry.energy < creep.carryCapacity) {
+      var sources = creep.room.find(FIND_SOURCES);
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+      }
     }
-  }
-  else {
-    if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
-      creep.moveTo(Game.spawns['Spawn1']);
+    else {
+      if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.spawns['Spawn1']);
+      }
     }
   }
 }
