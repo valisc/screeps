7d2650861b32423a619742fe1d013f96ab28cf6b
Valis C <valis@inkanito.net>
2016-12-04 14:27:53 -0500

    Transfer energy

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 module.exports.loop = function () {
   var creep = Game.creeps['Harvester1'];
-  var sources = creep.room.find(FIND_SOURCES);
-  if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-    creep.moveTo(sources[0]);
+
+  if(creep.carry.energy < creep.carryCapacity) {
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+      creep.moveTo(sources[0]);
+    }
+  }
+  else {
+    if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+      creep.moveTo(Game.spawns['Spawn1']);
+    }
   }
 }
-
