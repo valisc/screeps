c49e829852ec37b2d9512092df219e84d12ef92c
Valis C <valis@inkanito.net>
2016-12-17 13:03:56 -0500

    Send harvester to provide for structures

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
@@ -8,8 +8,8 @@ module.exports.loop = function () {
     if(creep.memory.role == 'harvester') {
       roleHarvester.run(creep);
     }
-    if(creep.memory.role == 'upgrader') {
-      roleUpgrader.run(creep);
+    if(creep.memory.role == 'builder') {
+      roleBuilder.run(creep);
     }
   }
 }
