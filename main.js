64d175fe776e5703b4f526380151d67e1f51d55d
Valis C <valis@inkanito.net>
2016-12-17 13:18:08 -0500

    Clear memory of dead screeps

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
 
+  for(var name in Memory.creeps) {
+    if(!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log('Clearing non-existing creep memory:', name);
+    }
+  }
+
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   console.log('Harvesters: ' + harvesters.length);
 
