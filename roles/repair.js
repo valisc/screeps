2cc52d35adf12bbd0af2f60b5cf3a14310b3a4f0
Valis C <valis@inkanito.net>
2016-12-19 03:16:22 -0500

    Add repair role (untested)

---
+++ roles/repair.js
@@ -0,0 +1,36 @@
+var roleRepair = {
+
+  /** @param {Creep} creep **/
+  run: function(creep) {
+
+    if(creep.memory.repairing && creep.carry.energy == 0) {
+      creep.memory.repairing = false;
+      creep.say('harvesting');
+    }
+    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.repairing = true;
+      creep.say('repairing');
+    }
+
+    if(creep.memory.repairing) {
+      var damagedStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: function(object){
+        return object.structureType === STRUCTURE_ROAD && (object.hits < object.hitsMax / 3);
+      }});
+        
+        
+      if(damagedStructure) {
+        if(creep.repair(damagedStructure) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(damagedStructure);
+        }
+      }
+    }
+    else {
+      var sources = creep.room.find(FIND_SOURCES);
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+      }
+    }
+  }
+};
+
+module.exports = roleRepair;
